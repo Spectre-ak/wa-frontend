1e4ac59d95293455d0b523617f8330276fcc3dda
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-05-09 08:28:32 +0530

    Add files via upload

--- src/recommendations/Recommendations.js
+++ src/recommendations/Recommendations.js
@@ -3,17 +3,53 @@ import ReactDOM from 'react-dom';
 import MaxResources from './MaxResources';
 import SkillsCheckBox from './SkillsCheckBOX'
 import $ from 'jquery';
+import UserProfile from '../UserList'
+import { render } from '@testing-library/react';
+import Loader from '../Loader';
 
-function doPostRequest(data) {
+function doPostRequest(data,res) {
+    ReactDOM.render(<Loader/>,document.getElementById("recommendationsResults"));
+
     $.ajax({
-        url: "http://localhost:8080/getV",
-       // url:"https://woay-backend.azurewebsites.net/recommendations",
+       // url: "http://localhost:8080/getV",
+        url:"https://woay-backend.azurewebsites.net/recommendations",
         type: "post",
         data: (data),
         contentType: false,
         processData: false,
         success: function (response) {
-            console.log(response);
+            //console.log(response);
+            //console.log(res);
+            
+//console.log(response["Senior"]!==undefined)
+
+            //ReactDOM.render(<UserProfile data={res}/>,document.getElementById("engrResc"));
+            var renderedData=[]; var data2=[];
+            
+            if(response["Senior"]!==undefined)
+                data2=data2.concat(response["Senior"])
+
+             //   renderedData.push(<UserProfile data={response["Senior"]}/>);
+
+            if(response["Mid"]!==undefined)
+            data2=data2.concat(response["Mid"])
+
+             //   renderedData.push(<UserProfile data={response["Mid"]}/>);
+
+            if(response["Junior"]!==undefined)
+            data2=data2.concat(response["Junior"])
+
+            //    renderedData.push(<UserProfile data={response["Junior"]}/>);
+
+            if(response.length!==undefined)
+                data2=data2.concat(response)
+
+           //     renderedData.push(<UserProfile data={response}/>);
+          //  console.log(renderedData)
+            if(data2.length!==0)
+                ReactDOM.render(<UserProfile data={data2}/>,document.getElementById("recommendationsResults"));
+            else 
+            ReactDOM.render(<a>No results found</a>,document.getElementById("recommendationsResults"));
 
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
@@ -22,7 +58,7 @@ function doPostRequest(data) {
     });
 }
 
-
+// IL (2) ["Senior", "Mid"] 2021-06-09 
 
 
 function DateComponent() {
@@ -63,17 +99,17 @@ class Recommendations extends React.Component {
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1Engr" value="option1"
                             ></input>
-                        <label class="form-check-label" for="inliddndeRadio1Engr">IL</label>
+                        <label class="form-check-label" for="inliddndeRadio1">IL</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2UX" value="option2"
                              />
-                        <label class="form-check-label" for="inlinefdfgRadio2UX">TX</label>
+                        <label class="form-check-label" for="inlinefdfgRadio2">TX</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3PM" value="option3"
                              />
-                        <label class="form-check-label" for="inlineRadgfdio3PM">AZ</label>
+                        <label class="form-check-label" for="inlineRadgfdio3">AZ</label>
                     </div>
                 </div>
 
@@ -147,7 +183,7 @@ class Recommendations extends React.Component {
 
         console.log(location,res,document.getElementById("prject-date-input").value)
 
-        doPostRequest(data);
+        doPostRequest(data,res);
     }
 }
 
